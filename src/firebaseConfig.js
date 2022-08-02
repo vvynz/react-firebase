dc7b1e17ff454874162886b536ee3bd871f8cf6d
Vivian Zhang <vvynz12@gmail.com>
2022-08-02 18:40:58 -0400

    Create a config file for firebase and initialize the firebase sdks

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAvn0Lg6rmwhY3BosTWJp2zgPCrZIBALoY",
+  authDomain: "fb-frontend-6252e.firebaseapp.com",
+  projectId: "fb-frontend-6252e",
+  storageBucket: "fb-frontend-6252e.appspot.com",
+  messagingSenderId: "433928358847",
+  appId: "1:433928358847:web:36900e3b730bcf1c9fef0a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
