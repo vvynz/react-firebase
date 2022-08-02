9b04dda1d74a1681056bfd271728b36093899f6d
Vivian Zhang <vvynz12@gmail.com>
2022-08-02 18:23:18 -0400

    Remove default generated css code and add some margin between the input fields

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <input placeholder="Email" type="email" name="email" />
-      <input placeholder="Password" type="password" name="password" />
+    <div className="App-header">
+      <input 
+      className="input-field"
+      placeholder="Email" 
+      type="email" 
+      name="email" />
+      <input 
+      className="input-field"
+      placeholder="Password" 
+      type="password" 
+      name="password" />
     </div>
   );
 }
