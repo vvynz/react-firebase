1a66f82c3ccdcf37e275f9ce550825a3f2a5f0d3
Vivian Zhang <vvynz12@gmail.com>
2022-08-02 18:28:49 -0400

    Add onChange event handlers to the input fields and call the handleInput func. Call te handleSubmit func when button is clicked

--- src/App.js
+++ src/App.js
@@ -6,20 +6,24 @@ function App() {
   }
 
   const handleSubmit = () => {}
-  
+
   return (
     <div className="App-header">
       <input 
       className="input-field"
       placeholder="Email" 
       type="email" 
-      name="email" />
+      name="email" 
+      onChange={(e) => handleInput(e)} 
+      />
       <input 
       className="input-field"
       placeholder="Password" 
       type="password" 
-      name="password" />
-       <button>Sign Up</button>
+      name="password" 
+      onChange={(e) => handleInput(e)} 
+      />
+       <button onClick={handleSubmit}>Sign Up</button>
     </div>
   );
 }
