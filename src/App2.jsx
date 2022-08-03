c853c3c986fa005ca4bdcd3eb8806001bce770a8
Vivian Zhang <vvynz12@gmail.com>
2022-08-03 17:52:16 -0400

    Set up an input field for email and password and a submit button. Create a handleSubmit function to save the input data to state

--- src/App2.jsx
+++ src/App2.jsx
@@ -2,9 +2,19 @@ import { useState  } from "react";
 import './App.css';
 
 export default function App2() {
+  const [data, setData] = useState({});
+
+  const handleInput = (e) => {
+    let newInput = { [e.target.name]: e.target.value };
+
+    setData({...data, ...newInput});
+  }
+
   return(
     <div className="App-header">
-      <input />
+      <input name="email" placeholder="email" onChange={(e) => handleInput(e)}/>
+      <input name="password" placeholder="password" onChange={(e) => handleInput(e)}/>
+      <button>Submit</button>
     </div>
   )
 }
